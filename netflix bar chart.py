8bfbdfb9d6562efa621a76d84b9d71a6e938dd1e
Zuckareya Amin <aminzak1234@gmail.com>
2025-07-21 16:24:06 +0100

    submodule test

---
+++ netflix bar chart.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt  # Correct import
+
+data = pd.read_csv("netflix_titles.csv")
+
+# Count the number of entries for each type (e.g., Movie, TV Show)
+type_counts = data['type'].value_counts()
+
+# Plot
+plt.bar(type_counts.index, type_counts.values, width=0.5, color="orange")
+
+plt.xlabel('Type')
+plt.ylabel('Amount')
+plt.title('Number of Titles by Type')
+
+plt.show()
