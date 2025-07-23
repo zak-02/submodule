38bbeb95b965f3f9276bdd2e2b816004690f4220
Zuckareya Amin <aminzak1234@gmail.com>
2025-07-23 16:49:13 +0100

    yml change

--- netflix.py
+++ netflix.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt 
 
-data = pd.read_csv("netflix_titles.csv")
+data = pd.read_csv("submodule/netflixTitles.csv")
 
-# Count the number of entries for each type (e.g., Movie, TV Show)
+
 type_counts = data['type'].value_counts()
 
-# Plot
+
 plt.bar(type_counts.index, type_counts.values, width=0.5, color="orange")
 
 plt.xlabel('Type')
